93291479054dfe6fb90c7f1420a22e6bc6bb42ba
Gonzi <git@dergonzi.de>
2021-01-13 16:18:58 +0100

    update db con

--- handler/database.js
+++ handler/database.js
@@ -1,31 +1,32 @@
 let mysql = require('mysql');
 let util = require('util');
+const log = require('../handler/logging.js');
 
 let pool = mysql.createPool({
     connectionLimit: 100,
-    host: 'dev-gonzi.de',
+    host: '85.214.153.77',
     user: 'keywordbot',
     password: 'Juz2lZXTEat6xOFPMLNM',
     database: 'keywordbot',
-    charset: 'utf8mb4'
+    charset: 'utf8mb4',
 });
 
 pool.getConnection((err, connection) => {
     if (err) {
         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.')
+            log.error('Database connection was closed.');
         }
         if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.')
+            log.error('Database has too many connections.');
         }
         if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.')
+            log.error('Database connection was refused.');
         }
     }
-    if (connection) connection.release()
-    return
+    if (connection) connection.release();
+    return;
 });
 
-pool.query = util.promisify(pool.query)
+pool.query = util.promisify(pool.query);
 
 module.exports = pool;
