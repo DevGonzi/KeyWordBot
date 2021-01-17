f6046193d80e3897c4566afe84adee4960d9f4cc
Gonzi <git@dergonzi.de>
2021-01-17 03:27:13 +0100

    adjust server ID

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,20 +1,18 @@
-const Discord = require("discord.js");
-const config = require("../config.json");
+const Discord = require('discord.js');
+const config = require('../config.json');
 const log = require('../handler/logging.js');
 
 module.exports = async (client, guild) => {
     // guild commands
-    console.log("Joined a new guild: " + guild.name);
+    console.log('Joined a new guild: ' + guild.name);
 
-    client.user.setActivity(`$help | Servers: ${client.guilds.cache.size}`).catch(log.error);;
+    client.user.setActivity(`$help | Servers: ${client.guilds.cache.size}`).catch(log.error);
 
-
-    let sendGuild = client.guilds.cache.get('744193604787896382');
+    let sendGuild = client.guilds.cache.get('661287017161949216');
 
     if (guild) {
-        channel = sendGuild.channels.cache.get('752798339686465556');
+        channel = sendGuild.channels.cache.get('800189097339125812');
         if (channel) {
-
             const itemaddmsg = new Discord.MessageEmbed()
                 .setColor('#11ff00')
                 .setTitle('Bot **joined**!')
@@ -24,9 +22,9 @@ module.exports = async (client, guild) => {
                 .addField(`Server Owner ID: `, `${guild.ownerID}`, true)
                 .setTimestamp()
                 .setFooter(`Thanks for using me!`);
-                channel.send(itemaddmsg);
-            } else {
+            channel.send(itemaddmsg);
+        } else {
             console.log("There's no channel with that ID.");
-        };
-    };
+        }
+    }
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,43 +1,30 @@
-const Discord = require("discord.js");
-const config = require("../config.json");
-const pool = require("../handler/database.js");
-const log = require("../handler/logging.js");
+const Discord = require('discord.js');
+const config = require('../config.json');
+const pool = require('../handler/database.js');
+const log = require('../handler/logging.js');
 
 module.exports = async (client, guild) => {
-  // guild commands
+    // guild commands
 
-  console.log("Kicked from a guild: " + guild.name);
+    console.log('Kicked from a guild: ' + guild.name);
 
-  pool.query(
-    `DELETE FROM keyworddb WHERE keyworddb.guildId = ${guild.id} AND keyworddb.isGlobal = "0"`,
-    guildId,
-    function (err, res, fields) {
-      if (err) {
-        log.error(err);
-      }
+    pool.query(`DELETE FROM keyworddb WHERE keyworddb.guildId = ${guild.id} AND keyworddb.isGlobal = "0"`, guildId, function (err, res, fields) {
+        if (err) {
+            log.error(err);
+        }
+    });
+
+    client.user.setActivity(`$help | Servers: ${client.guilds.cache.size}`).catch(log.error);
+
+    let sendGuild = client.guilds.cache.get('661287017161949216');
+
+    if (guild) {
+        channel = sendGuild.channels.cache.get('800189097339125812');
+        if (channel) {
+            const itemaddmsg = new Discord.MessageEmbed().setColor('#0099ff').setTitle('Bot **kicked**!').addField(`Server name: `, `${guild.name}`).addField(`Server ID: `, `${guild.id}`).addField(`Server Owner ID: `, `${guild.ownerID}`).setTimestamp().setFooter(`Guild Owner ${guild.ownerID}`);
+            channel.send(itemaddmsg);
+        } else {
+            console.log("There's no channel with that ID.");
+        }
     }
-  );
-
-  client.user
-    .setActivity(`$help | Servers: ${client.guilds.cache.size}`)
-    .catch(log.error);
-
-  let sendGuild = client.guilds.cache.get("744193604787896382");
-
-  if (guild) {
-    channel = sendGuild.channels.cache.get("752798339686465556");
-    if (channel) {
-      const itemaddmsg = new Discord.MessageEmbed()
-        .setColor("#0099ff")
-        .setTitle("Bot **kicked**!")
-        .addField(`Server name: `, `${guild.name}`)
-        .addField(`Server ID: `, `${guild.id}`)
-        .addField(`Server Owner ID: `, `${guild.ownerID}`)
-        .setTimestamp()
-        .setFooter(`Guild Owner ${guild.ownerID}`);
-      channel.send(itemaddmsg);
-    } else {
-      console.log("There's no channel with that ID.");
-    }
-  }
 };
